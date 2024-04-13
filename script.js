46d0ebdafa59d432a70b79e05ba72406d57b2a6b
shader-boy <shader_boy@hotmail.com>
2024-04-13 13:18:35 +0100

    first version

---
+++ script.js
@@ -0,0 +1,74 @@
+class Queue {
+  constructor() {
+    this.items = {};
+    this.front = 0;
+    this.rear = 0;
+  }
+
+  enqueue(element) {
+    this.items[this.rear] = element;
+    this.rear++;
+  }
+
+  dequeue() {
+    const item = this.items[this.front];
+    delete this.items[this.front];
+    this.front++;
+    return item;
+  }
+
+  isEmpty() {
+    return this.rear - this.front === 0;
+  }
+
+  peek() {
+    return this.items[this.front];
+  }
+
+  size() {
+    return this.rear - this.front;
+  }
+
+  print() {
+    console.log(this.items);
+  }
+}
+
+class Queue2 {
+  constructor() {
+    this.items = {};
+    this.front = 0;
+    this.rear = 0;
+  }
+
+  enqueue(el) {
+    this.items[this.rear] = el;
+    this.rear++;
+  }
+
+  dequeue() {
+    const item = this.items[this.front];
+    delete this.items[this.front];
+    this.front++;
+    return item;
+  }
+
+  isEmpty() {
+    return this.rear - this.front === 0;
+  }
+
+  size() {
+    return this.rear - this.front
+  }
+}
+
+// const a = 2;
+// const b = '2'
+// console.log(a+b);
+// console.log(typeof a + b)
+
+localStorage.setItem('name', 'bob');
+localStorage.setItem('name2', 'samir');
+console.log(localStorage.getItem('name2'))
+
+document.cookie = 'name=bob; expires=' + new Date(2024, 0, 19).toUTCString()
---
+++ script2.js
@@ -0,0 +1,35 @@
+"use strict";
+
+// class ListNode {
+//   constructor(data) {
+//     this.data = data;
+//     this.next = null;
+//   }
+// }
+
+// class LinkedList {
+//   constructor(head = null) {
+//     this.head = head;
+//   }
+// }
+
+// const node1 = new ListNode(2);
+// const node2 = new ListNode(5);
+// node1.next = node2;
+
+// const list = new LinkedList(node1);
+
+// console.log(list.head.next.data);
+
+class ListNode {
+  constructor(data) {
+    this.data = data;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+  constructor(head = null) {
+    this.head = head;
+  }
+}
