05340a4382262a760239ba87c570f02ef53e2140
shader-boy <shader_boy@hotmail.com>
2024-04-13 16:21:05 +0100

    another program

--- script2.js
+++ script2.js
@@ -1,26 +1,5 @@
 "use strict";
 
-// class ListNode {
-//   constructor(data) {
-//     this.data = data;
-//     this.next = null;
-//   }
-// }
-
-// class LinkedList {
-//   constructor(head = null) {
-//     this.head = head;
-//   }
-// }
-
-// const node1 = new ListNode(2);
-// const node2 = new ListNode(5);
-// node1.next = node2;
-
-// const list = new LinkedList(node1);
-
-// console.log(list.head.next.data);
-
 class ListNode {
   constructor(data) {
     this.data = data;
@@ -37,7 +16,3 @@ class LinkedList {
 {
   secNum = 25;
 }
-
-const thirdNum = 3;
-
-const fourthNum = 4;
